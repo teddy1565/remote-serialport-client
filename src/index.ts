b9e4b112eccd2503563a5cbe41cd52bf639f338c
teddy1565 <teddy1565@gmail.com>
2024-04-07 05:26:18 +0800

    feat: 🎸 implemnet dist client class

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,36 @@
-import { Manager } from "socket.io-client";
+import { Manager, Socket } from "socket.io-client";
+import { OpenSerialPortOptions } from "./types/remote-serialport-types/src/serialport";
+import { RemoteSerialClientSocket } from "./module/remote-serialport-client";
+import { AbsRemoteSerialportClient } from "./types/remote-serialport-types/src/remote-serial-client.model";
+import { SocketServerSideEmitChannel, SocketServerSideEmitPayload } from "./types/remote-serialport-types/src/index";
 
-import { IRemoteSerialportClient } from "./index.d";
+export class RemoteSerialportClient extends AbsRemoteSerialportClient {
 
-export class RemoteSerialportClient implements IRemoteSerialportClient {
-
-    private client_manager: Manager;
-
-    private serialport_check_regexp: RegExp | string;
+    protected _socket: RemoteSerialClientSocket | null = null;
 
     /**
      *
      * @param server_host - Server Host, Example: ws://localhost:17991
      */
     constructor(server_host: string, serialport_check_regexp: RegExp | string = /^(\/dev\/tty(USB|AMA|ACM)|\/COM)[0-9]+$/) {
-        throw new Error("Not Implemented");
-        // this.client_manager = new Manager(server_host);
-        // this.serialport_check_regexp = serialport_check_regexp;
+        super(server_host, serialport_check_regexp);
     }
 
     /**
-     *
+     * Open Serialport Path, namespace serial path will override open_options path
      * @param namesapce - The namespace of the server, Example: /dev/ttyUSB0 or COM1...
-     * @returns
+     * @param open_options - Serial Port Open Options
+     * @returns - Socket Instance
      */
-    connect(namesapce: string) {
+    connect(namesapce: string, open_options: OpenSerialPortOptions): RemoteSerialClientSocket {
         if (namesapce.match(this.serialport_check_regexp) === null) {
             throw new Error("Invalid Serialport");
         }
-        return this.client_manager.socket(namesapce);
+        const socketIO_client_instance = this.client_manager.socket(namesapce);
+        this._socket = new RemoteSerialClientSocket(socketIO_client_instance, open_options);
+        return this._socket;
     }
 
     disconnect() {
-    }
-
-    on(event: string, callback: (...args: any[]) => void) {
     }
 }
